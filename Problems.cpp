d893855ee218a990416a842b5e0b88b07f6ae2da
Yashjai45 <109402531+Yashjai45@users.noreply.github.com>
2023-10-26 14:55:07 +0530

    Create Problems.cpp

---
+++ Problems.cpp
@@ -0,0 +1,81 @@
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+
+Node* kReverse(Node* head, int k) {
+    //base call
+    if(head == NULL) {
+        return NULL;
+    }
+    
+    //step1: reverse first k nodes
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < k ) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    
+    //step2: Recursion dekhlega aage ka 
+    if(next != NULL) {
+        head -> next = kReverse(next,k);
+    }
+    
+    //step3: return head of reversed list
+    return prev;
+}
+
+
+#include<vector>
+using namespace std;
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	int i = 0, j = 0;
+    vector<int> ans;
+    while(i<n && j<m) {
+    
+    if(arr1[i]==arr2[j])
+    {
+    	ans.push_back(arr1[i]);
+        i++;
+        j++;
+    }
+    else if (arr1[i] < arr2[j]) {
+  		i++;
+    }
+    else
+    {
+    	j++;
+    }
+    
+    }
+    return ans;
+}
+
+
+
+
+
+
+
+
