7aced9ebaad7f442bfd50735a616a1b21b6ba884
Yashjai45 <109402531+Yashjai45@users.noreply.github.com>
2023-10-26 14:56:39 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,71 @@
+#include<vector>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    
+    for(int i = 1; i<n; i++) {
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--) {
+            
+            if(arr[j] > temp) {
+                //shift
+                arr[j+1] = arr[j];
+            }
+            else { // ruk jao
+                break;
+            }
+            
+        }
+        //copy temp value
+        arr[j+1] = temp;  
+    } 
+}
+
+
+
+#include<vector>
+using namespace std;
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i = 1; i<n; i++) {
+        //for round 1 to n-1
+        bool swapped = false;
+        
+        for(int j =0; j<n-i; j++) {
+            
+            //process element till n-i th index
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+            
+        }
+        
+        if(swapped == false) {
+            //already sorted
+            break;
+        }
+           
+    }
+}
+
+
+
+
+#include<vector>
+using namespace std;
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i = 0; i < n-1; i++ ) {
+        int minIndex = i;
+        
+        for(int j = i+1; j<n; j++) {
+            
+            if(arr[j] < arr[minIndex]) 
+                minIndex = j;
+            
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
