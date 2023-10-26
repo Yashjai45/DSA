043f131c1809fff19158c134ea28208103246f2d
Yashjai45 <109402531+Yashjai45@users.noreply.github.com>
2023-10-26 14:58:47 +0530

    Create Linked_list.cpp

---
+++ Linked_list.cpp
@@ -0,0 +1,176 @@
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+
+Node* kReverse(Node* head, int k) {
+    //base call
+    if(head == NULL) {
+        return NULL;
+    }
+    
+    //step1: reverse first k nodes
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < k ) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    
+    //step2: Recursion dekhlega aage ka 
+    if(next != NULL) {
+        head -> next = kReverse(next,k);
+    }
+    
+    //step3: return head of reversed list
+    return prev;
+}
+class Solution
+{
+    private:
+    Node* reverse(Node* head) {
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    void insertAtTail(struct Node* &head, struct Node* &tail, int val) {
+        
+        Node* temp = new Node(val);
+        //empty list
+        if(head == NULL) {
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else
+        {
+            tail -> next = temp;
+            tail = temp;
+        }
+    }
+    
+    struct Node* add(struct Node* first, struct Node* second) {
+        int carry = 0;
+        
+        Node* ansHead = NULL;
+        Node* ansTail = NULL;
+        
+        while(first != NULL || second != NULL || carry != 0) {
+            
+            int val1 = 0;
+            if(first != NULL)
+                val1 = first -> data;
+                
+            int val2 = 0;
+            if(second !=NULL)
+                val2 = second -> data;
+            
+            
+            int sum = carry + val1 + val2;
+            
+            int digit = sum%10;
+            
+            //create node and add in answer Linked List
+            insertAtTail(ansHead, ansTail, digit);
+            
+            carry = sum/10;
+            if(first != NULL)
+                first = first -> next;
+            
+            if(second != NULL)
+                second = second -> next;
+        }
+        return ansHead;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* first, struct Node* second)
+    {
+        //step 1 -  reverse input LL
+        first = reverse(first);
+        second = reverse(second);
+        
+        //step2 - add 2 LL
+        Node* ans = add(first, second);
+        
+        //step 3 
+        ans = reverse(ans);
+        
+        return ans;
+    }
+};
+
+
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    class Node 
+    {
+        public:
+        int data;
+        Node* next;
+
+        Node(int data) 
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+    
+************************************************************/
+
+Node * uniqueSortedList(Node * head) {
+   	//empty List
+    if(head == NULL)
+        return NULL;
+    
+    //non empty list
+    Node* curr = head;
+    
+    while(curr != NULL) {
+        
+        if( (curr -> next != NULL) && curr -> data == curr -> next -> data) {
+            Node* next_next = curr ->next -> next;
+            Node* nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
+        }
+        else //not equal
+        {
+            curr = curr -> next;
+        }   
+    }
+    
+    return head; 
+}
+
